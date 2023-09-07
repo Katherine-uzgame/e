f0010e4735d7e97dcad01a84a1ef9b1fa606e37f
Katherine-uzgame <119307470+Katherine-uzgame@users.noreply.github.com>
2023-09-07 09:01:34 -0500

    Add files via upload

---
+++ ejercicio4.js
@@ -0,0 +1,6 @@
+let n1=  parseInt(prompt("Digite el primer numero:"))
+let n2=  parseInt(prompt("Digite el segundo numero:"))
+
+for (i=0(n1/n2));
+
+console.log("Los numeros primos entre esos numeros son: ")
