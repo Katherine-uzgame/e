af48c8a210ac31c628bed77854a7c08834d2cf7a
Katherine-uzgame <119307470+Katherine-uzgame@users.noreply.github.com>
2023-09-07 09:11:56 -0500

    Add files via upload

---
+++ ejercicio5.js
@@ -0,0 +1,6 @@
+let n1=  parseInt(prompt("Digite el primer numero:"))
+let n2=  parseInt(prompt("Digite el segundo numero:"))
+
+for (let i=0; n1+n2)
+
+console.log("La cantidad de elementos que tiene el vector es: ")
