304a8873b587a51729cd2a65b789f3391ec3939d
Katherine-uzgame <119307470+Katherine-uzgame@users.noreply.github.com>
2023-09-07 08:48:57 -0500

    Add files via upload

---
+++ ejercicio2 evaluaci.js
@@ -0,0 +1,6 @@
+let n1=  parseInt(prompt("Digite cuanto es el sueldo anual:"))
+let n2=  parseInt(prompt("Digite cuanta bonificación resive: "))
+
+var=multiplicacion(n1*n2)
+
+console.log("El total del sueldo es de:")(n1*n2)
