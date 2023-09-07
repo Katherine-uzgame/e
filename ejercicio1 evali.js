48f2d25f4f391bfd20020ef445bd3fac7743d566
Katherine-uzgame <119307470+Katherine-uzgame@users.noreply.github.com>
2023-09-07 08:47:25 -0500

    Add files via upload

---
+++ ejercicio1 evali.js
@@ -0,0 +1,4 @@
+let n1 =  parseInt(prompt("Cuantos trabajadores son: "))
+let n2 =  parseInt(prompt("Cuanto es el valor del salario: "))
+let n3 =  parseInt(prompt("Digite el nombre de cada trabajador: "))
+
